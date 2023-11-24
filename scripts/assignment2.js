f4b7b40614312f4dd56399417ea6f3731d5a20d3
zanadoman <domanseven@gmail.com>
2023-11-24 11:55:59 +0100

    .

--- scripts/assignment2.js
+++ scripts/assignment2.js
@@ -1,40 +1,69 @@
+var users = [];
+
+function updateAllUsers(Count) {
+
+    console.log(Count)
+    var allUsers = document.getElementById('allUsers');
+
+    var content = '<table>';
+    for (var i = 0; i < Count; i++) {
+
+        content += `
+            <tr>
+                <td><img src="${users[i].avatar}"></td>
+                <td>${users[i].id}</td>
+                <td>${users[i].first_name}</td>
+                <td>${users[i].last_name}</td>
+                <td>${users[i].email}</td>
+            </tr>
+        `;
+    }
+    content += '</table>';
+
+    allUsers.innerHTML = content;
+}
+
 function loadUsers() {
 
     var url = 'https://reqres.in/api/users';
     var xhr = new XMLHttpRequest();
-    var content = document.getElementById('content');
-
-    content.innerHTML = ''; //Reset the content of the content
 
     xhr.open('GET', url);
-    xhr.onreadystatechange = function() { //Handle state change
+    xhr.onreadystatechange = function() {
 
-        if (this.readyState !== 4) { //Not ready yet
+        if (this.readyState !== 4) {
             return;
         }
 
-        if (this.status === 200) { //Handle success
+        if (this.status === 200) {
 
-            var users = JSON.parse(this.response).data;
+            response = JSON.parse(this.response).data;
 
-            var contentString = '<table>'; //Generating content for content
-            for (var i = 0; i < users.length; i++) {
-
-                contentString += `
-                    <tr>
-                        <td><img src="${users[i].avatar}"></td>
-                        <td>${users[i].id}</td>
-                        <td>${users[i].first_name}</td>
-                        <td>${users[i].last_name}</td>
-                        <td>${users[i].email}</td>
-                    </tr>
-                `;
+            for (var i = 0; i < response.length; i++) {
+                users[i] = response[i]
             }
-            contentString += '</table>';
 
-            content.innerHTML = contentString; //Apply content
+            userCountSelector();
+            updateAllUsers(users.length);
         }
     }
 
     xhr.send(null);
+}
+
+function userCountSelector() {
+
+    var selector = document.getElementById('userCountSelector');
+
+    var options = `<option value="${users.length}">Összes felhasználó</option>`;
+    for (var i = 0; i < users.length - 1; i++) {
+
+        options += `<option value="${i + 1}">${i + 1}db felhasználó</option>`
+    }
+    selector.innerHTML = options;
+
+    selector.addEventListener("change", function(event) {
+
+        updateAllUsers(event.target.value);
+    });
 }
